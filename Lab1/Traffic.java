9d447d9bbce1b236e498d916fcb4897d684bcd78
Yesaswini Boppudi <78902551+YesaswiniBoppudi@users.noreply.github.com>
2021-02-16 22:05:45 +0530

    Add files via upload

---
+++ Lab1/Traffic.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class Traffic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   String str = "Green";
+  	   switch(str)
+  	   {
+  	   case "Red":
+  		System.out.println("STOP");
+  		break;
+  	   case "Yellow":
+  		   System.out.println("READY");
+  		   break;
+  	   case "Green":
+  		   System.out.println("GO");
+  		   break;
+  	   default:
+  		   System.out.println(" ");
+  	   }
+  	   
+     }
+
+}
