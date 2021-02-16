b80e5d60d308612dba9e5996db536926c110b85a
Yesaswini Boppudi <78902551+YesaswiniBoppudi@users.noreply.github.com>
2021-02-16 22:19:37 +0530

    Add files via upload

--- LAB3/StrToken.java
+++ LAB3/StrToken.java
@@ -1,1 +1,22 @@
+package lab2;
+import java.util.*;
+public class StrToken {
 
+	public static void main(String[] args) {
+		int num;
+        int sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter integers:");
+        String sc = s.nextLine();
+        StringTokenizer st = new StringTokenizer(sc, " ");
+        while (st.hasMoreTokens()) {
+            String temp = st.nextToken();
+            num = Integer.parseInt(temp);
+            System.out.println(num);
+            sum = sum + num;
+        }
+        System.out.println("sum of the integers is: " + sum);
+        s.close();
+	}
+
+}
