eb6d9eca17f72efca7b616bc79fcd7dc73bcb88a
Yesaswini Boppudi <78902551+YesaswiniBoppudi@users.noreply.github.com>
2021-02-16 22:04:44 +0530

    Add files via upload

--- Lab1/Cube.java
+++ Lab1/Cube.java
@@ -1,1 +1,16 @@
-
+package lab1;
+import java.util.*;
+import java.lang.*;
+class Cube {
+         public static int CubicSum(int x) 
+		    { 
+		        int sum = ((x*(x+1))/2); 
+		        return sum*sum; 
+		    }  
+		    public static void main(String[] args) 
+		    { 
+		        int x = 5; 
+		        System.out.println(CubicSum(x)); 
+		  
+		    }  
+	      }
