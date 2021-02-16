2b06a650399c6ce4d34b4f59f08f3956fb51aed0
Yesaswini Boppudi <78902551+YesaswiniBoppudi@users.noreply.github.com>
2021-02-16 22:13:23 +0530

    Add files via upload

--- LAB2/GetSecondSmallest.java
+++ LAB2/GetSecondSmallest.java
@@ -1,1 +1,14 @@
+package lab2;
+import java.util.*;
+public class GetSecondSmallest {
+public static int Smallest(int[] x,int Total) {
+	Arrays.sort(x);
+	return x[1];
+}
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x[]= {1,2,3,4,5,5};
+        System.out.println("Second Smallest number in array:"+Smallest(x,6));
+	}
 
+}
